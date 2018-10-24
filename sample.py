57553d7cba77578374d3b7ca27f8211d9080b802
Alex Kyungho Park <30945486+konkuest@users.noreply.github.com>
2018-10-24 21:30:47 +0900

    #1 Done
    세부데이터 부분 아직 미구현

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common import action_chains
@@ -35,8 +37,36 @@ def search_medicine(query):
     sleep(3)
     specific_result = driver.find_element_by_xpath("//a[@id='anchor_proy_more']") #검색결과 더보기 버튼\
     specific_result.click()
+
+    #10개보기를 100개보기로 확장
+    view_expansion = driver.find_element_by_xpath("//a[@id='line_selectBoxTitle']")
+    view_expansion.click()
+    hundred_list = driver.find_element_by_xpath("//li[@id='line_li_100']")
+    hundred_list.click()
+    sleep(2)
+
+# Step 3 : 세부 데이터를 수집하기
+
+# 인코딩 문제 해결 못함
+def get_info():
+    move_into_btns = driver.find_elements_by_xpath("//td[@class='txtL'][@style='cursor:pointer']") #len() 하면 100개 출력, 배열(list) 형태로 각 약 이름들이 들어가있음
+    move_into_btns[0].click() #100개 뜬 약물 이름들 중 첫 번째 것 CLICK
+    
+    #약의 이름
+    drug_data = driver.find_element_by_xpath("//td[@id='result_drug_name']").text
+    drug_name = str(drug_data).encode('utf-8')
     print('success')
+    print(type(drug_name))
+    print(drug_name)
+
+
     
 
 
+# Step 4 : for문 돌면서 약 리스트 돌기
+
+
+
 search_medicine('두통')
+get_info()
+
